46ff23c151597595d56514db434958372d5bdfe6
CHARDEVI <deviprasadisaka@gmail.com>
2023-03-28 19:36:45 +0530

    first commit

---
+++ devi/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def charishma(request):
+    return render(request,'charishma.html')
+def cherry(request):
+    return render(request,'cherry.html')
